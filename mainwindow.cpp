a769495077407a1537eb743e7ac9e64137cb7cc2
TLacmann <Hans-Hinterhuber@hotmail.de>
2013-03-12 17:06:14 +0100

    Changed XML structure and first XML Tests

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+//#include <c++/string>
+//#include <QString>
 
 
 
@@ -9,6 +11,27 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //QString School = Ui.cBSchool->itemData(0);
+    //QMessageBox.show(School);
+    /*
+    String schoolname = "AlbertEinsteinGymnasium-Ulm";
+    String XMLschoolFile = schoolname + "-xml";
+
+    QFile file (XMLschoolFile);
+    if(!file.open(IO_ReadOnly))
+    {
+        return; //cannot read file
+    }
+
+    QDomDocument doc;
+    if (!doc.setContent(&file))
+    {
+        return; //cannot parse file
+    }
+    */
+    int CountOfCourses = 2;
+    int CoursesArray[CountOfCourses];
 }
 
 MainWindow::~MainWindow()
@@ -23,6 +46,6 @@ void MainWindow::on_pBnewCoursSelection_clicked()
   //  MessageBox(HWND_DESKTOP,"Neue Kurswahl","Test",MB_OK)
   //  QTabBar *Tab;
   //  Tab->SelectRightTab;
-    schoolxmlarraybuild();
+  //schoolxmlarraybuild(CoursesArray[]);
 
 }
